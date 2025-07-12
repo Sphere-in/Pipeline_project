7572a41493fa81586582ee6368711ba7a49579c9
Sphere <spherein@hotmail.com>
2025-07-12 22:16:39 +0530

    2nd Commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,16 @@ provider "aws" {
 resource "aws_default_vpc" "default" {
 }
 
+variable "private_key_path" {
+  type        = string
+  description = "Path to the SSH private key file"
+}
 resource "aws_instance" "my_instance" {
   ami           = "ami-020cba7c55df1f615"
   instance_type = "t3.micro"
-  subnet_id = aws_default_vpc.default.default_subnet_id
+  subnet_id = aws_default_vpc.default.id
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  key_name = file(var.private_key_path)
   tags = {
     Name = "my-instance"
   }
@@ -35,4 +40,8 @@ resource "aws_volume_attachment" "my_attachment" {
   device_name = "dev/sdf"
   volume_id   = aws_ebs_volume.my_volume.id
   instance_id = aws_instance.my_instance.id
+}
+
+output "Instance_IP" {
+  value = aws_instance.my_instance.public_ip
 }
