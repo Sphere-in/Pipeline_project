ec73c61e45dd8cc2eb49bf9e3fd5e4c009bd4ab6
Sphere <spherein@hotmail.com>
2025-07-13 13:02:13 +0530

    Fixing Error -16

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,19 +33,19 @@ resource "aws_instance" "my_instance" {
   }
 }
 
-resource "aws_ebs_volume" "my_volume" {
-  availability_zone = aws_instance.my_instance.availability_zone
-  size              = 8
-  tags = {
-    Name = "my-volume"
-  }
-}
+# resource "aws_ebs_volume" "my_volume" {
+#   availability_zone = aws_instance.my_instance.availability_zone
+#   size              = 8
+#   tags = {
+#     Name = "my-volume"
+#   }
+# }
 
-resource "aws_volume_attachment" "my_attachment" {
-  device_name = "/dev/xvdf"
-  volume_id   = aws_ebs_volume.my_volume.id
-  instance_id = aws_instance.my_instance.id
-}
+# resource "aws_volume_attachment" "my_attachment" {
+#   device_name = "/dev/xvdf"
+#   volume_id   = aws_ebs_volume.my_volume.id
+#   instance_id = aws_instance.my_instance.id
+# }
 
 output "instance_ip" {
   value = aws_instance.my_instance.public_ip
